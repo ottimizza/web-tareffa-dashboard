958a52fcb168bd08f5dd21d337b13995b463ac7a
Rodrigo <rodrigo.h.m.112200@gmail.com>
2020-06-04 12:02:03 -0300

    Bugfix: The IndicatorService was sending a JSON of JSONs to the backend

--- src/app/shared/utils/string.utils.ts
+++ src/app/shared/utils/string.utils.ts
@@ -1,12 +1,10 @@
 import { ElementRef, Injectable } from '@angular/core';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class StringUtils {
-
-    public static normalize(text: string): string {
-        return text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-    }
-
+  public static normalize(text: string): string {
+    return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
 }
